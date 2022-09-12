511951e72a624e0f1a88bc8a54dd8d3a3275e7db
Walker <walker@praiseweb.com.br>
2022-09-12 19:28:58 -0300

    feat: add patat

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Plutus Experience";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell { buildInputs = [ pkgs.haskellPackages.patat ]; };
+      });
+}
