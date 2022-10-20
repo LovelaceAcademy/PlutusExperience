fc1e83753dd3ba0b90e7a579a6f5739557ebab87
Walker <walker@praiseweb.com.br>
2022-10-20 18:48:15 -0300

    build(m01): change from cabal2nix to haskell.nix

--- modules/M01-plutus-emulator/flake.nix
+++ modules/M01-plutus-emulator/flake.nix
@@ -1,36 +1,58 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = inputs@{ self, nixpkgs, flake-utils  }:
-    flake-utils.lib.eachDefaultSystem (system:
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        haskellPackages = pkgs.haskell.packages.ghc92;
-
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-
-        packageName = "M01-plutus-emulator";
-      in {
-        packages.${packageName} =
-          haskellPackages.callCabal2nix packageName ./${packageName}.cabal rec {
-            # Dependency overrides go here
-          };
-
-        # We don't need to build anything to nix, so skipping that
-        # defaultPackage = self.packages.${system}.${packageName};
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            haskellPackages.haskell-language-server # you must build it with your ghc to work
-            ghcid
-            cabal-install
-          ];
-          inputsFrom = builtins.attrValues self.packages.${system};
+        name = "M01-plutus-emulator";
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            ${name} =
+              final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc8107";
+                # This is used by `nix develop .` to open a shell for use with
+                # `cabal`, `hlint` and `haskell-language-server`
+                shell.tools = {
+                  cabal = { };
+                  # hlint = { };
+                  haskell-language-server = { };
+                };
+                # Non-Haskell shell tools go here
+                shell.buildInputs = with pkgs; [
+                  # nixpkgs-fmt
+                ];
+                # This adds `js-unknown-ghcjs-cabal` to the shell.
+                # shell.crossPlatforms = p: [p.ghcjs];
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.${name}.flake {
+          # This adds support for `nix build .#js-unknown-ghcjs:hello:exe:hello`
+          # crossPlatforms = p: [p.ghcjs];
         };
+      in
+      flake // {
+        # Built by `nix build .`
+        packages.default = flake.packages."${name}:lib";
       });
+
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    allow-import-from-derivation = "true";
+  };
 }
