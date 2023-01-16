d0fde3f1f19a16245bde2b256e246f0ba102b927
Walker <walker@praiseweb.com.br>
2023-01-16 11:51:05 -0300

    refact(m07): use mkEval helper

--- modules/M07-jupiter/src/Wallet.purs
+++ modules/M07-jupiter/src/Wallet.purs
@@ -1,6 +1,6 @@
 module Wallet (component) where
 
-import Prelude (($), (<$>), (+), unit, pure, bind, discard, show)
+import Prelude (($), (<$>), (+), unit, bind, show, const)
 import Control.Monad.State (get, modify_)
 import Data.Maybe (Maybe (Nothing, Just))
 import Data.Foldable (foldM)
@@ -16,18 +16,15 @@ type State = { api :: WA.Api, balance :: Maybe Number }
 component :: forall q o m.  MonadEffect m => MonadAff m => H.Component q WA.Api o m
 component = H.mkComponent {
   initialState: \api -> { api, balance: Nothing },
-  eval: case _ of
-    (H.Initialize a) -> do
+  eval: H.mkEval $ H.defaultEval {
+    initialize = Just unit,
+    handleAction = const do
       { api } <- get
       utxos <- liftAff $ WAA.getUtxos api
       balance <- liftEffect $ foldM (\acc utxo -> (+) acc <$> (WA.coin $ WA.amount $ WA.output utxo)) 0.0 utxos
       modify_ $ _ { balance = Just balance }
-      pure a
-    (H.Finalize a) -> pure a
-    (H.Receive _ a) -> pure a
-    (H.Action _ a) -> pure a
-    (H.Query _ fn) -> pure $ fn unit
-  , render: case _ of
+  },
+  render: case _ of
     { balance: (Just value) } -> HH.div_ [
       HH.span_ [HH.text "Balance: "],
       HH.span_ [HH.text $ show value]
